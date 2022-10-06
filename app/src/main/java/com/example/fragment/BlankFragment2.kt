2fe95a0b1ff7296bebaafb2cf6923e94c662fc3c
HIDDENPOOL <110696633+RAHIDDENPOOL@users.noreply.github.com>
2022-10-06 22:02:04 +0300

    upd

--- app/src/main/java/com/example/fragment/BlankFragment2.kt
+++ app/src/main/java/com/example/fragment/BlankFragment2.kt
@@ -1,16 +1,11 @@
 package com.example.fragment
 
-import android.content.ContentValues.TAG
-import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-
-class PonasenkovFragment : Fragment(R.layout.fragment_ponasenkov) {
-
+class BlankFragment2 : Fragment(R.layout.fragment_blank2) {
 
 }
--- app/src/main/java/com/example/fragment/MainActivity.kt
+++ app/src/main/java/com/example/fragment/MainActivity.kt
@@ -4,11 +4,37 @@ import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button1: Button = findViewById(R.id.btnOne)
+        val button2: Button = findViewById(R.id.btnTw)
+
+        val firstFragment = PonasenkovFragment()
+        val secondFragment = BlankFragment2()
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.frag, firstFragment)
+            addToBackStack(null)
+            commit()
+        }
+        button1.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.frag, firstFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
+        button2.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.frag, secondFragment)
+                commit()
+            }
+        }
+
 
         Log.d(TAG, "onCreate?")
     }
